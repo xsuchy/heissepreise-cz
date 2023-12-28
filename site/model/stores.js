58a850d300155eafe6698acadda81d9de127f773
Jan Elzein <elzein.eltom@gmail.com>
2023-12-28 16:32:07 +0100

    Albert samostatně

--- site/model/stores.js
+++ site/model/stores.js
@@ -47,6 +47,13 @@ exports.stores = {
         defaultChecked: true,
         getUrl: (item) => `https://www.dm.cz/product-p${item.url}`,
     },
+    albert: {
+        name: "Albert",
+        budgetBrands: [],
+        color: undefined,
+        defaultChecked: true,
+        getUrl: (item) => `https://www.albert.cz${item.url}`,
+    },
 };
 
 exports.STORE_KEYS = Object.keys(exports.stores);
