85bca7a27f070a34c3e87f685d75d6e70d3a6d4b
Jan Elzein <elzein.eltom@gmail.com>
2024-01-03 23:44:50 +0100

    shop.iglobus.cz/cs/search?sort=price_asc&ipp=72&page=X#, ale chybí kategorie

--- site/model/stores.js
+++ site/model/stores.js
@@ -52,7 +52,7 @@ exports.stores = {
         budgetBrands: [],
         color: undefined,
         defaultChecked: true,
-        getUrl: (item) => `https://shop.iglobus.cz/cz/${item.url}`,
+        getUrl: (item) => `https://shop.iglobus.cz${item.url}`,
     },
 };
 
